da4909d3ea28e71a2a11af574339ce78079b54d3
Chris Jimenez <cjimenez16@gmail.com>
2016-02-25 21:28:00 -0600

    initial commit

---
+++ Podfile
@@ -0,0 +1,24 @@
+platform :ios, '8.0'
+use_frameworks!
+
+def test_pods
+     pod 'Quick', '~> 0.8.0'
+     pod 'Nimble', '~> 3.0.0'
+end
+
+target 'RevealingSplashView' do
+
+end
+
+target 'RevealingSplashViewTests' do
+    test_pods
+end
+
+target 'RevealingSplashViewSample' do
+
+end
+
+target 'RevealingSplashViewSampleTests' do
+
+end
+
---
+++ RevealingSplashViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RevealingSplashViewSample
+//
+//  Created by Chris Jimenez on 2/25/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
