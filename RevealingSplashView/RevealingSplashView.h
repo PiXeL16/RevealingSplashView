de7720a5a3999e24b4f765a0ac7796bda8aae9dc
Chris Jimenez <cjimenez16@gmail.com>
2016-02-25 21:22:27 -0600

    Initial Commit

---
+++ RevealingSplashView/RevealingSplashView.h
@@ -0,0 +1,19 @@
+//
+//  RevealingSplashView.h
+//  RevealingSplashView
+//
+//  Created by Chris Jimenez on 2/25/16.
+//  Copyright © 2016 Chris Jimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RevealingSplashView.
+FOUNDATION_EXPORT double RevealingSplashViewVersionNumber;
+
+//! Project version string for RevealingSplashView.
+FOUNDATION_EXPORT const unsigned char RevealingSplashViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RevealingSplashView/PublicHeader.h>
+
+
