3e53faf16b3bfdb484d54a9b3885002576802621
Reza Moghtaderi <reza@cirtual.com>
2016-09-23 13:24:11 -0500

    Revert "Demo App for the Heart Beat Animation"
    This reverts commit 7345996363aa280d409753261b1e8e5bf1cd4254.

--- RevealingSplashViewSample/ViewController.swift
+++ RevealingSplashViewSample/ViewController.swift
@@ -11,37 +11,28 @@ import RevealingSplashView
 
 class ViewController: UIViewController {
 
-    let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "twitterLogo")!,iconInitialSize: CGSize(width: 70, height: 70), backgroundColor: UIColor(rgba:"#1D8FF1"))
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "twitterLogo")!,iconInitialSize: CGSize(width: 70, height: 70), backgroundColor: UIColor(rgba:"#1D8FF1"))
         
         self.view.addSubview(revealingSplashView)
         
-        revealingSplashView.duration = 3
+        revealingSplashView.duration = 0.9
         
-        revealingSplashView.iconColor = UIColor.white
+        revealingSplashView.iconColor = UIColor.red
         revealingSplashView.useCustomIconColor = false
         
-        revealingSplashView.animationType = SplashAnimationType.HeartBeat
-
+        revealingSplashView.animationType = SplashAnimationType.SwingAndZoomOut
+    
         revealingSplashView.startAnimation(){
             
             print("Completed")
         }
-
-        // Simulate a network call finishing in 30 seconds... (think loading PokemonGo!)
-        Timer.scheduledTimer(timeInterval: 13, target: self, selector: #selector(update), userInfo: nil, repeats: false)
-   
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    // stop the heart beat animation
-    func update(){
-        revealingSplashView.finishHeartBeatAnimation()
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
